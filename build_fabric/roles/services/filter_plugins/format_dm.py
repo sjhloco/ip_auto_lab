22c32f4de8e908a8318f6f8b27d2cfd8f46ac720
mucholoco <mucholoco@hotmail.co.uk>
2020-05-24 14:00:38 +0100

    filter without custom ports

--- build_fabric/roles/services/filter_plugins/format_dm.py
+++ build_fabric/roles/services/filter_plugins/format_dm.py
@@ -2,7 +2,7 @@ class FilterModule(object):
     def filters(self):
         return {
             'create_svc_tnt_dm': self.svc_tnt_dm,
-            # 'srv_ports_dm': self.srv_ports_dm
+            'create_svc_intf_dm': self.svc_intf_dm
         }
 
     # Creates 2 new seperate Data Models for Leaf and Border devices with only the tenants and vlans on those device roles and incorporating the VNIs
@@ -63,6 +63,114 @@ class FilterModule(object):
 
         return [leaf_tnt, border_tnt]
 
+    def svc_intf_dm(self, all_types, hostname, intf_adv, bse_intf):
+        sl_hmd = intf_adv['single_homed']
+        dl_hmd = intf_adv['dual_homed']
+
+        intf_fmt = bse_intf['intf_fmt']
+        ec_fmt = bse_intf['ec_fmt']
+
+        #
+        all_interfaces = []
+
+        # 1. DEFAULTS: Fill out default values for dual_homed & po_mode, add stp & port type and create new list
+        for intf_type, interfaces in all_types.items():
+            for intf in interfaces:
+                if intf.setdefault('dual_homed', True) != False:
+                    intf.setdefault('po_mode', 'active')
+                # Adds port type as dict and combines into 1 non-nested list
+                if intf_type == 'layer3':
+                    intf['intf_type'] = 'layer3'
+                if intf_type == 'access':
+                    intf['stp'] = 'edge'
+                    intf['intf_type'] = 'access'
+                elif intf_type == 'stp_trunk':
+                    intf['stp'] = 'network'
+                    intf['intf_type'] = 'stp_trunk'
+                elif intf_type == 'non_stp_trunk':
+                    intf['stp'] = 'normal'
+                    intf['intf_type'] = 'non_stp_trunk'
+                all_interfaces.append(intf)
+
+        # 2. FILTER: Deletes interface DM for interfaces not on this switch (single-homed) or the MLAG pair (dual-homed)
+        # for interfaces in all_types.values():
+        #     # Reverse loops through list so doesnt have any iteration issues due to loop elements being deleted
+        #     for intf in reversed(interfaces):
+        #         if intf['dual_homed'] == False and intf['switch'] != hostname:
+        #             interfaces.remove(intf)
+        # # Needs fresh loop as if hostname is even changes name (-1) so keeps interface config for the MLAG pair
+        # for interfaces in all_types.values():
+        #     for intf in reversed(interfaces):
+        #         if intf['dual_homed'] == True:
+        #             if int(hostname[-2:]) % 2 == 0:
+        #                 hostname = hostname[:-2] + "{:02d}".format(int(hostname[-2:]) -1)
+        #             if intf['switch'] != hostname:
+        #                 interfaces.remove(intf)
+        #         del intf['switch']                  # Removes as no longer needed
+
+        # # 3. SINGLE-HOMED: Adds the interface number to existing DM
+        # sl_incr_num, dl_incr_num = (0 for i in range(2))
+        # for intf_type, interfaces in all_types.items():
+        #     port_channels = []
+        #     for intf in interfaces:
+        #         if intf['dual_homed'] == False:
+        #             intf['num'] = intf_fmt + str(sl_hmd['first_intf'] + sl_incr_num)
+        #             sl_incr_num += 1
+        #         # 4. DUAL-HOMED: Adds the interface number, creates port-channel DM and adds PO and VPC to existing DM
+        #         elif intf['dual_homed'] == True:
+        #             intf['num'] = intf_fmt + str(dl_hmd['first_intf'] + dl_incr_num)
+        #             intf['vpc_num'] = dl_hmd['vpc'] + dl_incr_num
+        #             intf['po_num'] = dl_hmd['po'] + dl_incr_num
+        #             dl_incr_num += 1
+        #             port_channels.append({'num': ec_fmt + str(intf['po_num']), 'descr': intf['descr'], 'ip_vlan': intf['ip_vlan'], 'stp': intf['stp']})
+        #         del intf['dual_homed']                  # Removes as no longer needed
+        #     interfaces.extend(port_channels)            # Adds PO to list of interfaces
+
+
+        return all_interfaces
+
+# interfaces = [{'descr': 'dev1'}, {'descr': 'dev2'}, {'descr': 'dev3'}, {'descr': 'dev4'}, {'descr': 'dev5'}, {'descr': 'dev6'}]
+# first_intf = 13
+# last_intf = 32
+# int_num = [1,2,3,4,5,6]
+
+# for intf, num in zip(interfaces, range(first_intf, last_intf)):
+#     intf['num'] = num
+
+# if n in range(3,9)
+
+
+# if interface is defined get the numbers.
+# check if numbers are in the range.
+# Create a new list of all numbers in range excluding these
+# double loop using rnage numbers.
+
+
+#     access:
+#       - descr: DC1-SRV-APP01 eth1
+#         ip_vlan: 10
+#         switch: DC1-N9K-LEAF01
+#       - descr: DC1-SRV-PRD01 eth1
+#         ip_vlan: 20
+#         switch: DC1-N9K-LEAF01
+
+
+#   adv:                  # Reserved interface range that server ports can be automatically assigned from (applies to all leaf and border switches)
+#     dual_homed:                   # Used only for dual-homed devices
+#       first_int: 21     # First interface
+#       last_int: 50      # Last interface
+#       po: 21                      # First PortChannel used
+#       vpc: 21                     # First VPC used
+#     single_homed:                 # Used only for single-homed devices
+#       first_int: Ethernet1/51     # First interface
+#       last_int: Ethernet1/60      # Last interface
+
+
+
+        # l3vni = bse_vni['l3vni']
+        # tnt_vlan = bse_vni['tnt_vlan']
+        # l2vni = bse_vni['l2vni']
+        # border_tnt, leaf_tnt = ([] for i in range(2))
 
 
 
